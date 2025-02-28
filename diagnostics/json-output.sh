62bec31b9cce2e23de327317a7d469638f8b3c16
jesse goodier <31039225+jessegoodier@users.noreply.github.com>
2025-02-28 08:12:59 -0500

    bingen to json

--- diagnostics/json-output.sh
+++ diagnostics/json-output.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Accept Optional Namespace -- default to kubecost
-# Accept Optional Container -- default to cost-model otherwise aggregator
-# Accept Optional Data Type -- default to assets otherwise allocations
-# Accept Optional jq -- parse with jq otherwise print raw
+# This script will exec into a kubecost pod and output the json for a given data type
+# It will default to the cost-model container and assets data type if no arguments are provided
+# It will output raw json by default, or pipe to jq if the --jq flag is provided
+
 # usage: ./json-output.sh [--namespace namespace] [--container container] [--data-type data_type] [--jq]
 # example: ./json-output.sh --namespace kc26 --container aggregator --jq --data-type allocations
 
@@ -54,40 +54,35 @@ if [ -z "$namespace" ]; then
   fi
 fi
 
-echo "Namespace: $namespace"
-
-# Find pod running $container container
-echo "Finding pod running $container container..."
 pod=$(kubectl get pods -n "$namespace" -o json | jq -r ".items[] | select(.spec.containers[].name == \"$container\") | .metadata.name")
 
-if [ -n "$pod" ]; then
-    echo "Found pod running $container: $pod"
-else
+if ! [ -n "$pod" ]; then
     echo "No pod found running $container container in namespace $namespace"
     exit 1
 fi
 
 latest_file=$(kubectl exec -i "$pod" -c $container -n "$namespace" -- ls -1t /var/configs/db/etl/bingen/$data_type/1d/ |head -n1 )
 
-if [ -n "$latest_file" ]; then
-    echo "Latest file found: $latest_file"
-else
+if ! [ -n "$latest_file" ]; then
     echo "No files found in /var/configs/db/etl/bingen/$data_type/1d/"
     exit 1
 fi
 my_file="/var/configs/db/etl/bingen/$data_type/1d/$latest_file"
-echo "my_file: $my_file"
 
-echo "Running: kubectl exec -n "$namespace" "$pod" -c $container -- /go/bin/app bingentojson "$my_file""
-
-my_output=$(kubectl exec -n "$namespace" "$pod" -c $container -- /go/bin/app bingentojson "$my_file")
+my_output=$(kubectl exec --quiet -n "$namespace" "$pod" -c $container -- /go/bin/app bingentojson "$my_file")
 # echo "my_output: $my_output"
 # Remove all text before the first {
 my_json=$(echo "$my_output" | sed '0,/{/s/^[^{]*//')
 
 if [ "$run_jq" == "true" ]; then
-  echo "$my_json" | jq
+  echo "$my_json" 
 else
+  echo "Namespace: $namespace"
+  echo "Finding pod running $container container..."
+  echo "Found pod running $container: $pod"
+  echo "Latest file found: $latest_file"
+  echo "my_file: $my_file"
+  echo "Running: kubectl exec --quiet -n "$namespace" "$pod" -c $container -- /go/bin/app bingentojson "$my_file""
   echo "$my_json"
 fi
 
